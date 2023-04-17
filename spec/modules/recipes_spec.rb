93ee3ffad01a2524a2a7ff6012753a1b89343822
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-17 23:53:38 +0500

    test for the recipes

---
+++ spec/modules/recipes_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+    before do
+        @user = User.create(name: 'John')
+        @recipe = Recipe.create(name: 'Chicken',preparation_time: 30, cooking_time: 20, description: "Chicken is good" ,
+            public: true ,user_id: @user.id)
+    end
+    it "is valid with valid attributes" do
+        expect(@recipe).to be_valid
+    end
+    it "name is present" do
+        expect(@recipe.name).to be_present
+    end
+    it "name should be equal to Chicken" do
+        expect(@recipe.name).to eq("Chicken")
+    end
+    it "preparation_time is present" do
+        expect(@recipe.preparation_time).to be_present
+    end
+    it "preparation_time should be equal to 30" do
+        expect(@recipe.preparation_time).to eq(30)
+    end
+    it "Recipes is public" do
+        expect(@recipe.public).to eq(true)
+    end
+end
