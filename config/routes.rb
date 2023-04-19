020345a4ac0173464724c6108623800627c08e61
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-19 17:57:00 +0500

    setup the Public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'recipes/', to: 'recipes#index'
-  get 'recipes/:id', to: 'recipes#show'
-  delete 'recipes/:id', to: 'recipes#destroy'
+  # get 'recipes/', to: 'recipes#index'
+  # get 'recipes/:id', to: 'recipes#show'
+  # delete 'recipes/:id', to: 'recipes#destroy'
 
 
   root "foods#index"
 
   resources :foods ,only: [:index, :new, :create, :destroy]
+  resources :publicrecipes, only: [:index]
+  # resources :recipes, only: [:index, :show, :destroy, :public_recipes] 
 end
