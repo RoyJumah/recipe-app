69d583f0de8d5b3385573f2807063f877cab96fc
RoyJumah <royjumah17@gmail.com>
2023-04-19 12:30:59 +0300

    add public checkbox and form to create recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,15 +3,35 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
   def show
-    @recipe_foods = RecipeFood.where(recipe_id: params[:id])
     @foods = Food.all
     @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.user_id = 1
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+    else
+      render :new
+      flash[:alert] = 'Recipe was not created.'
+    end
   end
 
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
