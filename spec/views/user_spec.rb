c755b65d78d3c5efbf64285685cf20fa0c164d89
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-21 12:55:27 +0500

    Fix linter

--- spec/views/user_spec.rb
+++ spec/views/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "UserSignsIn", type: :system do
+RSpec.feature 'UserSignsIn', type: :system do
   let(:user) { create(:user) }
 
   it 'with valid credentials' do
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
