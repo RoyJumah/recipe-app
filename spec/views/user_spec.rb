4bfd67b763f0edc20b1cff1e29938f54093e5b61
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-21 09:55:24 +0500

    Add a test for food view

---
+++ spec/views/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "UserSignsIn", type: :system do
+  let(:user) { create(:user) }
+
+  it 'with valid credentials' do
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    expect(page).to have_content('Signed in successfully.')
+  end
+end
